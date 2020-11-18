ce26d9849a6e40dbc2cec282a511a618640f6158
wulingbing <wulingbing1998@163.com>
2020-11-18 12:08:30 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/wlb/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.wlb.git;
+
+/**
+ * author: wlb:Xb17620119
+ * creat: 2020-11-18 11:51
+ * description: description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
